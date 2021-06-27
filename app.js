8d409259a76b35da804f89f51046d2672634b7f0
Alli Tjader <allisontj@zillowgroup.com>
2021-06-26 23:44:02 -0700

    reduced to functions and added a secret menu

--- app.js
+++ app.js
@@ -1,21 +1,31 @@
-//let userName = 'Alli'
-
-//console.log(userName)
-
-let userName = prompt ("Please enter your name: "); 
-console.log(userName); 
-document.write('<h2>' + userName + '</h2>'); 
-
-let userAnswer = prompt ("Do you like pie? "); 
-console.log(userAnswer); 
-
-if (userAnswer == 'yes') {
-    document.write('<h3>We are glad you are here, ' + userName + '!');
-} else {
-    document.write('<h3>Maybe you will change your mind!');
+function getUserName(){
+    let userName = prompt ("Please enter your name: "); 
+    console.log(userName); 
+    document.write('<h2>' + userName + '</h2>'); 
+    return userName;
 }
 
-if (confirm("Please select yes to continue to our website")) {
+function getUserAnswer(){
+    let userAnswer = prompt ("Do you like pie?"); 
+    console.log(userAnswer); 
+    
+    if (userAnswer.toLowerCase() == 'yes') {
+        document.write('<h3>We are glad you are here!</h3>');
+    } else {
+        document.write('<h3>Maybe you will change your mind after reading <a href="https://www.foodandwine.com/desserts/pies/five-reasons-why-pie-best">this</a>!');
+    }
+}
+
+function getDessertAnswer(){
+    let dessertAnswer = prompt("Is pie your favorite dessert?"); 
+    if (dessertAnswer.toLowerCase() == 'yes'){
+        document.write ('<h3>Secret Pie Menu</h3>');
+        document.write ('<ul style="list-style-type:none"><li>Pie Flavor 1</li><li>Pie Flavor 2</li></ul>')
+    } 
+}
+
+
+if (confirm("Please select 'ok' to continue to our website")) {
     Text = "Confirmed!"; 
 } else {
     Text = "You pressed cancel!"; 
