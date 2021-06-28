b1fa07b0642c9ff05521707754a72b37659026c8
Alli Tjader <allisontj@zillowgroup.com>
2021-06-27 23:31:34 -0700

    Added pie picture and repeated image prompt

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+    let url ="https://tastesbetterfromscratch.com/wp-content/uploads/2015/11/Berry-Pie-4-2.jpg"
+
 function getUserName(){
     let userName = prompt ("Please enter your name: "); 
     console.log(userName); 
@@ -8,9 +10,9 @@ function getUserName(){
 function getUserAnswer(){
     let userAnswer = prompt ("Do you like pie?"); 
     console.log(userAnswer); 
-    
     if (userAnswer.toLowerCase() == 'yes') {
         document.write('<h3>We are glad you are here!</h3>');
+        document.write('<img src="' + url + '"/>')
     } else {
         document.write('<h3>Maybe you will change your mind after reading <a href="https://www.foodandwine.com/desserts/pies/five-reasons-why-pie-best">this</a>!');
     }
@@ -24,9 +26,42 @@ function getDessertAnswer(){
     } 
 }
 
-
 if (confirm("Please select 'ok' to continue to our website")) {
     Text = "Confirmed!"; 
 } else {
     Text = "You pressed cancel!"; 
+}
+
+// function userGuessingGame(){
+//     correctAnswer = 10; 
+//     userAttempts = 5; 
+//     for(let i = 0; i < userAttempts; i++){
+//         let userGuess = prompt("Please enter a number between 1 and 20");
+//         while(userGuess < 1 || userGuess > 20){
+//             userGuess = prompt("Incorrect range - please enter a number between 1 and 20");
+//         }
+//         if (userGuess == correctAnswer){
+//             alert('Correct!');
+//             break;
+//         } else if(userGuess < correctAnswer){
+//             alert('Too low - try again')
+//         } else if (userGuess> correctAnswer){
+//             alert('Too high - try again')
+//         } else {
+//             alert('Something else went wrong'); 
+//         }
+//         if (i == userAttempts - 1){
+//             alert('You did not guess the correct answer. The correct answer is ' + correctAnswer); 
+//         }
+//     }
+// }
+
+// userGuessingGame();
+
+function piePicture(){
+    let numberOfPie = prompt('How many pies would you like to see?')
+    for(let i = 0; i < numberOfPie; i ++){
+        document.write('<img src="' + url + '" >')
+        document.write('Pie picture number ' + (i + 1))
+    }
 }
